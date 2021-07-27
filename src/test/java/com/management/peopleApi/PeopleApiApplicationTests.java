122361279c1f726ebf319c8e3ab30bfc3b8c4dcd
Lucas-Dinelli <dinealbuquerque10@gmail.com>
2021-07-27 10:52:54 -0300

    Initial commit

---
+++ src/test/java/com/management/peopleApi/PeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.peopleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
